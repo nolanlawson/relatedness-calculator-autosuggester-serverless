e0def73771e8a08c4130d83100d71c4b5620a82f
Nolan Lawson <nolan@nolanlawson.com>
2021-11-07 14:40:06 -0800

    fix: add cache

--- src/main/java/autosuggester/App.java
+++ src/main/java/autosuggester/App.java
@@ -3,6 +3,7 @@ package autosuggester;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
@@ -14,6 +15,9 @@ import com.nolanlawson.relatedness.autosuggest.RelationSuggester;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import com.google.common.base.Function;
+import com.google.common.collect.MapMaker;
+
 /**
  * Handler for requests to Lambda function.
  */
@@ -23,6 +27,18 @@ public class App implements RequestHandler<APIGatewayProxyRequestEvent, APIGatew
     private static final int MAX_NUM_SUGGESTIONS = 10;
     private static final RelationSuggester relationSuggester = new RelationSuggester();
     private static final Gson gson = new GsonBuilder().disableHtmlEscaping().create();
+
+    /**
+     * Use a small LRU cache to hold the autosuggest data
+     */
+    private static final Map<String, List<String>> cache = new MapMaker()
+            .maximumSize(1000)
+            .makeComputingMap(
+                    new Function<>() {
+                        public List<String> apply(String q) {
+                            return relationSuggester.suggest(q, MAX_NUM_SUGGESTIONS);
+                        }
+                    });
 
     public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
         Map<String, String> headers = new HashMap<>();
@@ -36,7 +52,7 @@ public class App implements RequestHandler<APIGatewayProxyRequestEvent, APIGatew
                 throw new RuntimeException("Length too large: " + q.length());
             }
 
-            List<String> suggestions = relationSuggester.suggest(q, MAX_NUM_SUGGESTIONS);
+            List<String> suggestions = cache.get(q);
             Map<String, List<String>> results = new HashMap<>();
             results.put("results", suggestions);
             String output = gson.toJson(results);
